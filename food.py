46a5789f795cb0ddfd5cd6fd8fd0593a9d4ba190
Aniketh H <61650767+aniketh2007@users.noreply.github.com>
2021-11-28 17:37:23 +0530

    Add files via upload

---
+++ food.py
@@ -0,0 +1,21 @@
+# How to render itself on the screen as a small circle, Whenever the snake touches the food it is going to move to a
+# new random location
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-260, 260)
+        random_y = random.randint(-260, 260)
+        self.goto(random_x, random_y)
